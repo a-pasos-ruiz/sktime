525bff61fb47781aeb65217a9af0eed532373541
a-pasos-ruiz <fbu19zru@uea.ac.uk>
2022-02-25 10:28:28 +0000

    Added new dimension selection methdos

--- sktime/transformations/panel/dev/__init__.py
+++ sktime/transformations/panel/dev/__init__.py
@@ -2,9 +2,15 @@
 """Rocket transformers."""
 __all__ = [
     "DSRocket",
-    "DSRandom"
+    "DSRandom",
+    "ecs",
+    "kmeans",
+    "ecp"
 ]
 
 from sktime.transformations.panel.dev._rocket_selection import DSRocket
 from sktime.transformations.panel.dev._ds import DimensionSelection
 from sktime.transformations.panel.dev._random_ds import RandomDimensionSelection
+from sktime.transformations.panel.dev.cs import ecs
+from sktime.transformations.panel.dev.cs import kmeans
+from sktime.transformations.panel.dev.cs import ecp
